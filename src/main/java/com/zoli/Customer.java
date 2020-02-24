3c72068dfefb1df28f8075994c86cc53a14140bf
Zoltan Kovacs <>
2020-02-24 20:46:49 +1100

    added bullshit code

---
+++ src/main/java/com/zoli/Customer.java
@@ -0,0 +1,31 @@
+package com.zoli;
+
+public class Customer {
+
+    private int id;
+    private String name;
+
+    public Customer(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
---
+++ src/main/java/com/zoli/MyController.java
@@ -0,0 +1,21 @@
+package com.zoli;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MyController {
+
+
+    @RequestMapping("/")
+    public List<Customer> findAll()
+    {
+        List<Customer> customerList = new ArrayList<Customer>();
+        customerList.add(new Customer(1, "frank"));
+        customerList.add(new Customer(2, "john"));
+        return customerList;
+    }
+}
