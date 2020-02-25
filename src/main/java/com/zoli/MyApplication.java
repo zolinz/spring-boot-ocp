78db04cbda52af7ee2efc5b18622bca70dc7b94c
Zoltan Kovacs <>
2020-02-25 16:38:34 +1100

    test pipeline

--- src/main/java/com/zoli/MyApplication.java
+++ src/main/java/com/zoli/MyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello zolikam");
         SpringApplication.run(MyApplication.class, args);
     }
 }
